17f9b1070438f81374640261c4961cfad33ded6b
jdomfern <118214384+jdomfern@users.noreply.github.com>
2022-12-19 19:30:54 +0100

    Update Main1.java

--- ProyectoGitRamasEjemplo/src/Main1.java
+++ ProyectoGitRamasEjemplo/src/Main1.java
@@ -4,6 +4,11 @@ public class Main1 {
 	public static void main(String[] args) {
 		// Ejercicio 1
 		System.out.println("Hello MUNDO");
+		
+		public static void imprimeSaludo() { 
+System.out.println(“Hei”);	
+}
+
 	}
 
 }
